b908346bca2f061dec4fca82e2d0a7983b52ef5f
Nathalie A. Vladis <nathalievladis@gmail.com>
2020-01-19 14:49:21 -0500

    Create install.py

---
+++ install.py
@@ -0,0 +1,11 @@
+import os, itertools 
+import numpy as np 
+import pandas as pd
+
+from skimage.transform import resize
+ 
+import matplotlib.pyplot as plt 
+%matplotlib inline
+
+import pathlib
+import pickle
